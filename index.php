113245439291e504f28c4cd7ad5a47c5cc88b8c8
Rizky Arinanda AR <RizkyWebID@GMail.Com>
2016-05-06 02:46:07 +0700

    XAMPP-Local
    Membuat Dashboard Sederhana Dari HTML Dan PHP, Untuk Melihat Isi XAMPP
    Dan Web Server Virtual Lainnya

---
+++ index.php
@@ -0,0 +1,108 @@
+<html lang='id'>
+	<head>
+		<title>XAMPP Templates By RizkyWebID</title>
+		<link rel='stylesheet' href='css/bootstrap.min.css'>
+		<link rel='stylesheet' href='css/XAMPP-Templates.css'>
+		<link rel='stylesheet' href='css/bootstrap-theme.min.css'>
+	</head>
+	<body>
+		<header class='well well-sm'>
+			<h3 align='center'>Selamat Datang Di Panel XAMPP Localhost</h4>
+		</header>
+		<br/>
+		<article class='container'>
+			<div class='row'>
+				<div class='col-lg-4 col-sm-6 text-center'>
+					<br/>
+					<a href='/dashboard'>
+						<img class='img-circle img-responsive img-center' src='img/Dashboard.jpg' alt='Dashboard'>
+						<h3>Dashboard</h3>
+					</a>
+					<br/>
+				</div>
+				<div class='col-lg-4 col-sm-6 text-center'>
+					<br/>
+					<a href='/webalizer'>
+						<img class='img-circle img-responsive img-center' src='img/Webalizer.jpg' alt='Webalizer'>
+						<h3>Webalizer</h3>
+					</a>
+					<br/>
+				</div>
+				<div class='col-lg-4 col-sm-6 text-center'>
+					<br/>
+					<a href='/phpmyadmin'>
+						<img class='img-circle img-responsive img-center' src='img/PhpMyAdmin.png' alt='PhpMyAdmin'>
+						<h3>PhpMyAdmin</h3>
+					</a>
+					<br/>
+				</div>
+				<div class='col-lg-4 col-sm-6 text-center'>
+					<br/>
+					<a href='/Data-Others'>
+						<img class='img-circle img-responsive img-center' src='img/Data-Others.jpg' alt='Data-Others'>
+						<h3>Data-Others</h3>
+					</a>
+					<br/>
+				</div>
+				<div class='col-lg-4 col-sm-6 text-center'>
+					<br/>
+					<a href='/Developers'>
+						<img class='img-circle img-responsive img-center' src='img/Developers.jpg' alt='Developers'>
+						<h3>Developers</h3>
+					</a>
+					<br/>
+				</div>
+				<div class='col-lg-4 col-sm-6 text-center'>
+					<br/>
+					<a href='/Project-Ku'>
+						<img class='img-circle img-responsive img-center' src='img/Project-Ku.gif' alt='Project-Ku'>
+						<h3>Project-Ku</h3>
+					</a>
+					<br/>
+				</div>
+			</div>
+		</article>
+		<footer>
+			<ul align='center' class='list-inline'>
+				<li>
+					<a href='#' class='btn-social btn-outline'>
+						<img class='img-circle img-responsive img-center' src='img/Icon-Facebook.png' alt='Facebook'>
+					</a>
+				</li>
+				<li>
+					<a href='#' class='btn-social btn-outline'>
+						<img class='img-circle img-responsive img-center' src='img/Icon-Github.png' alt='Github'>
+					</a>
+				</li>
+				<li>
+					<a href='#' class='btn-social btn-outline'>
+						<img class='img-circle img-responsive img-center' src='img/Icon-Google.png' alt='Google'>
+					</a>
+				</li>
+				<li>
+					<a href='#' class='btn-social btn-outline'>
+						<img class='img-circle img-responsive img-center' src='img/Icon-Instagram.png' alt='Instagram'>
+					</a>
+				</li>
+				<li>
+					<a href='#' class='btn-social btn-outline'>
+						<img class='img-circle img-responsive img-center' src='img/Icon-Twitter.png' alt='Twitter'>
+					</a>
+				</li>
+				<li>
+					<a href='#' class='btn-social btn-outline'>
+						<img class='img-circle img-responsive img-center' src='img/Icon-Wordpress.png' alt='Wordpress'>
+					</a>
+				</li>
+				<li>
+					<a href='#' class='btn-social btn-outline'>
+						<img class='img-circle img-responsive img-center' src='img/Icon-Youtube.png' alt='Youtube'>
+					</a>
+				</li>
+			</ul>
+			<h4 align='center'>Copyright &copy; XAMPP Templates - <a href='http://Rizky.Web.ID/'>Rizky.Web.ID</a> 2016</h4>
+		</footer>
+		<script src='js/jquery.min.js'></script>
+		<script src='js/bootstrap.min.js'></script>
+	</body>
+</html>
